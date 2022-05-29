fdf44507cb8114e7ecab80638d42733c36bbe047
Taylor Lowery <taylorlowery07@gmail.com>
2022-05-29 12:57:33 -0700

    no longer looking for specific chapter haders in XML

--- copyit.sh
+++ copyit.sh
@@ -1,16 +1,2 @@
 #!/bin/sh
 mkdir /media/usb0/book
-cp D:\Projects\Python\mp3splitter\tests\test_files\The Story for Children-Part02_000_Chapter_15_00--split.mp3 /media/usb0/book/
-cp D:\Projects\Python\mp3splitter\tests\test_files\The Story for Children-Part02_001_Chapter_16_00--split.mp3 /media/usb0/book/
-cp D:\Projects\Python\mp3splitter\tests\test_files\The Story for Children-Part02_002_Chapter_17_00--split.mp3 /media/usb0/book/
-cp D:\Projects\Python\mp3splitter\tests\test_files\The Story for Children-Part02_003_Chapter_18_00--split.mp3 /media/usb0/book/
-cp D:\Projects\Python\mp3splitter\tests\test_files\The Story for Children-Part02_004_Chapter_19_00--split.mp3 /media/usb0/book/
-cp D:\Projects\Python\mp3splitter\tests\test_files\The Story for Children-Part02_005_Chapter_20_00--split.mp3 /media/usb0/book/
-cp D:\Projects\Python\mp3splitter\tests\test_files\The Story for Children-Part02_006_Chapter_21_00--split.mp3 /media/usb0/book/
-cp D:\Projects\Python\mp3splitter\tests\test_files\The Story for Children-Part02_007_Chapter_22_00--split.mp3 /media/usb0/book/
-cp D:\Projects\Python\mp3splitter\tests\test_files\The Story for Children-Part02_008_Chapter_23_00--split.mp3 /media/usb0/book/
-cp D:\Projects\Python\mp3splitter\tests\test_files\The Story for Children-Part02_009_Chapter_24_00--split.mp3 /media/usb0/book/
-cp D:\Projects\Python\mp3splitter\tests\test_files\The Story for Children-Part02_010_Chapter_25_00--split.mp3 /media/usb0/book/
-cp D:\Projects\Python\mp3splitter\tests\test_files\The Story for Children-Part02_011_Chapter_26_00--split.mp3 /media/usb0/book/
-cp D:\Projects\Python\mp3splitter\tests\test_files\The Story for Children-Part02_012_Chapter_27_00--split.mp3 /media/usb0/book/
-cp D:\Projects\Python\mp3splitter\tests\test_files\The Story for Children-Part02_013_Chapter_28_00--split.mp3 /media/usb0/book/
